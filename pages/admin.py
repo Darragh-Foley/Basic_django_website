6d56b9ce650ed081c6279f5f94b41906f6993394
Darragh Foley <darragh.foley34@mail.dcu.ie>
2024-03-16 17:22:31 +0000

    project update

--- pages/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Trail, Review, MagicPage
-# Register your models here.
-
-admin.site.register(Trail)
-admin.site.register(Review)
-admin.site.register(MagicPage)
-
